afdce912aed337bc73cfbd959c416b01fe4bc1ec
Holger Gauss <holger.gauss@sap.com>
2019-01-25 12:27:41 +0100

    Changed the HTML for the site.

--- hg1_blog/settings.py
+++ hg1_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f)i+cs%w+6uyh7i$y@_*5)ugr&vw1&3iim)sne==^@-i+25w+^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
